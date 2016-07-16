6f2a1da7fa8cca23df55cb4d6ac866b9097b8bd6
Samir A. Aleido <eidosam@hotmail.com>
2016-07-16 19:47:27 +0200

    use express

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var http = require('http');
+// var http = require('http');
 
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+// function handleRequest(request, response){
+//     response.end('It Works!! Path Hit: ' + request.url);
+// }
 
-var server = http.createServer(handleRequest);
+// var server = http.createServer(handleRequest);
  
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
 });
+
+app.listen(server_port);
+
+// server.listen(server_port, server_ip_address, function () {
+//   console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+// });
