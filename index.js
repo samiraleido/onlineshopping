22db0b9008d45ed8941c329460282c89c42b7115
Samir A. Aleido <eidosam@hotmail.com>
2016-07-16 14:20:54 +0200

    update server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+var express = require('express');
+var app = express();
+
+var mongojs = require('mongojs');
+var database = mongojs('eidosoft_db_admin:notpass@ds055862.mlab.com:55862/eidosoft_db?authMechanism=SCRAM-SHA-1', ['Products']);
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(process.env.NODE_PORT || 3000);
